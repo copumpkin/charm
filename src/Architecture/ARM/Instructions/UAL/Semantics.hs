eeece5aa0492d6f5ce0619e752f1614d77b2c80d
Daniel Peebles <pumpkingod@gmail.com>
2010-09-08 07:42:09 +0200

    A first glimpse at what semantics might look like, using the state structure. Need a state monad, but it would be parametrized by two types? Needs thought

--- src/Architecture/ARM/Instructions/UAL/Semantics.hs
+++ src/Architecture/ARM/Instructions/UAL/Semantics.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE GADTs #-}
 module Architecture.ARM.Instructions.UAL.Semantics where
   
+import Architecture.ARM.State
 import Architecture.ARM.Instructions.UAL
 
--- This should be put somewhere central
+import Data.Record.Label
+
+-- This should be put somewhere else, maybe even in a separate package
 data Expr a where
   Const  :: a -> Expr a
   (:+)   :: Expr a -> Expr a -> Expr a
@@ -16,4 +19,15 @@ data Expr a where
   (:>>>) :: Expr a -> Expr a -> Expr a
   (:&)   :: Expr a -> Expr a -> Expr a
   (:|)   :: Expr a -> Expr a -> Expr a
-  
+
+-- Num should be an expression, and we should steal all the operators
+-- Need state monad
+evaluateConditional :: Num r => Conditional -> ARMState r m -> ARMState r m
+evaluateConditional (B off) = modL pc (+ fromIntegral off)
+evaluateConditional (BX r)  = do x <- getL (reg r)
+                                 setL pc x
+                                 -- set arm/thumb bit
+evaluateConditional x = error $ "haven't implemented semantics for conditional instruction " ++ show x
+
+evaluate :: UALInstruction -> ARMState r m -> ARMState r m
+evaluate x = error $ "haven't implemented semantics for instruction " ++ show x
